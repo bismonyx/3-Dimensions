7a4f92b77dcf5702d908805d827ac8d8a9c760ef
Prince <princerana8@gmail.com>
2025-05-07 21:04:42 -0400

    Able to hit the buffalo and tiger

---
+++ 3d-game/tigerr.gd
@@ -0,0 +1,11 @@
+extends AnimatableBody3D
+@export var max_health = 4
+var health = 0
+
+func _ready() -> void:
+	health = max_health
+
+func take_dmg(amount):
+	health = health - amount 
+	if health <= 0:
+		queue_free()
